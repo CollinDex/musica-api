aacf28c0c856053bcdce13c11ef40566c0619efd
CollinDex <obetscollins@gmail.com>
2025-04-03 00:19:46 +0100

    chore: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { SeedService } from './modules/seed/seed.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  //Configure Swagger Module
+  const config = new DocumentBuilder()
+    .setTitle('Musica')
+    .setDescription('Musica Api Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const seedService = app.get(SeedService);
   //await seedService.seed();
   await app.listen(process.env.PORT || 3000);
