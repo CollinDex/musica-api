b1780a91816943908e43ec56764a800eaa0df753
CollinDex <obetscollins@gmail.com>
2025-04-03 01:13:56 +0100

    chore: add types for swagger documentaion

--- src/common/types/auth-types.ts
+++ src/common/types/auth-types.ts
@@ -1,3 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export type Enable2FAType = {
   secret: string;
 };
+
+export class FASecret {
+  @ApiProperty({
+    example: 'MZHDG6ZKF43XMQDLIZFT6MCTJBVVORK3IERXMNKMIZZUMORDLM4Q',
+    description: '2fa secret',
+  })
+  secret: 'MZHDG6ZKF43XMQDLIZFT6MCTJBVVORK3IERXMNKMIZZUMORDLM4Q';
+}
+
+export class LoginResponse {
+  @ApiProperty({
+    example: 'asdfalsdkjflk4roiwekjsdofleaskdmflkadmsflkamdsf',
+    description: 'access_code',
+  })
+  access_code: 'asdfalsdkjflk4roiwekjsdofleaskdmflkadmsflkamdsf;ojads;flkjm';
+}
